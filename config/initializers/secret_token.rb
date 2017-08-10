5aec092f4b3052591cea3bd10e7d98f180006c79
akash-bansal <akash.bansal@flipkart.com>
2017-08-10 23:36:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendy::Application.config.secret_token = '6a18368f63f66bf0f92332dbc255d52ec420fa6fd6f446ef83a6ebf3dcac002e79d5ec6e557fa53b535896a5c60688e2b6156567289231ab2f7d82826ac4b2e1'
