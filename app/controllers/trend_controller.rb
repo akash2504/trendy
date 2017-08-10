926922dd5365ac494c529a7f6237de76ea78081a
akash-bansal <akash.bansal@flipkart.com>
2017-08-11 02:42:53 +0530

    changes

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -1,8 +1,8 @@
 require 'restclient'
 class TrendController < ApplicationController
   def home
-    searchTerms="shirt"
-    @trends=RestClient.get('http://172.20.44.29:3000/sizzles/search?tags='+searchTerms)
+    searchTerms="shirt,clothing"
+    @trends=JSON.parse(RestClient.get('http://172.20.44.29:3000/sizzles/search?tags='+searchTerms))
     @trends
   end
 end
